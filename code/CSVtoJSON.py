0b37489aeda4d8e1444da0ce13b3818bd8e40a0b
Jikke1995 <jikke.vdende@hotmail.com>
2019-01-10 14:49:42 +0100

    update

---
+++ code/CSVtoJSON.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# Name: Jikke van den Ende
+# Student number: 10787593
+"""
+This script converts a CSV file into a JSON file.
+"""
+
+import csv
+import pandas as pd
+import json
+
+INPUT_CSV = "plastic-waste-generation-total (2).csv"
+OUTPUT_JSON = "data.json"
+
+def convert_into_json_file(data):
+    """
+    This writes a JSON file with given CSV data.
+    """
+    # Open an empty json file
+    jsonfile = open(OUTPUT_JSON, 'w')
+
+    # Write csv data into new json file
+    out = json.dumps(data)
+    jsonfile.write(out)
+
+    return jsonfile
+
+def select_data(file):
+    """
+    This function selects the data of interest from the csv file.
+    """
+    # Read csv file
+    reader = csv.DictReader(file)
+    print(reader)
+    data = {}
+    # In this case, selects the data from Australia, the number of Alcohol
+    # Consumption for the years 1960 to 2015.
+    for row in reader:
+        print(row['Code'])
+        data[row['Code']] = row["Plastic waste generation (tonnes, total) (tonnes per year)"]
+
+    return data
+
+if __name__ == "__main__":
+    csv_file = open(INPUT_CSV, 'rU')
+    data = select_data(csv_file)
+    jsonfile = convert_into_json_file(data)
---
+++ code/project.js
@@ -0,0 +1,15 @@
+/**
+Name: Jikke van den Ende
+Student number: 10787593
+This file contains the script for the linked views HTML. It creates a datamap
+of the world, which shows (for countries with available data) the alcohol
+consumption per country for the year 2015. When clicked on a country, a barchart
+is shown which shows the alcohol consumption over multiple years for that
+country.
+*/
+
+window.onload = function() {
+
+  var map = new Datamap({element: document.getElementById('container')});
+
+};
